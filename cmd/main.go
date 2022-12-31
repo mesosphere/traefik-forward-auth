66c7c4eff3e8e1c300971d46f1bbe592d078b55c
Efra Peralta <efra23peralta@gmail.com>
2022-12-30 22:52:05 -0400

    chore: remove kubernetes

--- cmd/main.go
+++ cmd/main.go
@@ -7,16 +7,12 @@ import (
 	"time"
 
 	"github.com/gorilla/sessions"
-	k8s "k8s.io/client-go/kubernetes"
-
-	"github.com/turnly/oauth-middleware/internal/api/storage/v1alpha1"
+	api "github.com/turnly/oauth-middleware/internal/api"
 	"github.com/turnly/oauth-middleware/internal/authentication"
 	"github.com/turnly/oauth-middleware/internal/configuration"
 	"github.com/turnly/oauth-middleware/internal/handlers"
-	kubernetes "github.com/turnly/oauth-middleware/internal/kubernetes"
 	logger "github.com/turnly/oauth-middleware/internal/log"
 	"github.com/turnly/oauth-middleware/internal/storage"
-	"github.com/turnly/oauth-middleware/internal/storage/cluster"
 )
 
 // Main
@@ -40,54 +36,30 @@ func main() {
 	}
 
 	authenticator := authentication.NewAuthenticator(config)
-	// Get clientset for Authorizers
-	var clientset k8s.Interface
-	if config.EnableRBAC || config.EnableInClusterStorage {
-		var err error
-		clientset, err = kubernetes.GetClientSet()
-		if err != nil {
-			log.Fatalf("error getting kubernetes client: %v", err)
-		}
+	var store api.StoreInterface
+
+	// Prepare cookie session store (first key is for auth, the second one for encryption)
+	hashKey := []byte(config.SecretString)
+	blockKey := []byte(config.EncryptionKeyString)
+	cookieStore := sessions.NewCookieStore(hashKey, blockKey)
+	cookieStore.Options.MaxAge = int(config.Lifetime / time.Second)
+	cookieStore.Options.HttpOnly = true
+	cookieStore.Options.Secure = !config.InsecureCookie
+
+	store = &storage.GorillaUserInfoStore{
+		SessionStore: cookieStore,
+		SessionName:  config.ClaimsSessionName,
+		Auth:         authenticator,
 	}
 
-	var userInfoStore v1alpha1.UserInfoInterface
-	if !config.EnableInClusterStorage {
-		// Prepare cookie session store (first key is for auth, the second one for encryption)
-		hashKey := []byte(config.SecretString)
-		blockKey := []byte(config.EncryptionKeyString)
-		cookieStore := sessions.NewCookieStore(hashKey, blockKey)
-		cookieStore.Options.MaxAge = int(config.Lifetime / time.Second)
-		cookieStore.Options.HttpOnly = true
-		cookieStore.Options.Secure = !config.InsecureCookie
-
-		userInfoStore = &storage.GorillaUserInfoStore{
-			SessionStore: cookieStore,
-			SessionName:  config.ClaimsSessionName,
-			Auth:         authenticator,
-		}
-	} else {
-		userInfoStore = cluster.NewClusterStore(
-			clientset,
-			config.ClusterStoreNamespace,
-			config.SecretString,
-			config.Lifetime,
-			time.Duration(config.ClusterStoreCacheTTL)*time.Second,
-			authenticator)
-
-		gc := cluster.NewGC(userInfoStore.(*cluster.ClusterStorage), time.Minute, false, true)
-
-		if err := gc.Start(); err != nil {
-			log.Fatalf("error starting GC process: %v", err)
-		}
-	}
 	// Build server
-	server := handlers.NewServer(userInfoStore, clientset, config)
+	server := handlers.NewServer(store, config)
 
 	// Attach router to default server
 	http.HandleFunc("/", server.RootHandler)
 
-	// Start
-	log.Debugf("starting with options: %s", config)
-	log.Info("listening on :4181")
+	// Start server
+	log.Debugf("Starting OAuth Middleware with the following configuration: %s", config)
+	log.Info("Ahoy! The OAuth Middleware is now listening on port 4181")
 	log.Info(http.ListenAndServe(":4181", nil))
 }
